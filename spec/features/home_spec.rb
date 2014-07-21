a60b5a49d6fed683f07b54ed00d807dec76db614
Nicola Rankin <nicola.rankin@gmail.com>
2014-07-21 23:03:01 +1200

    add ability to edit profile

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -4,15 +4,25 @@ Warden.test_mode!
 
 
 feature 'home page' do
-  scenario 'has a logout link' do
-    @user = FactoryGirl.create(:user)
-    login_as(@user, :scope => :user)
-    visit root_path
-    expect(page).to have_link('Log Out')
+  context 'when a user is logged in ' do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      login_as(@user, :scope => :user)
+    end
+    scenario 'it has a logout link' do
+      visit root_path
+      expect(page).to have_link('Log Out')
+    end
+    scenario 'it has a link to edit profile' do
+       visit root_path
+      expect(page).to have_link('Edit profile')
+    end
   end
-  scenario 'has a login link' do
-    visit root_path
-    expect(page).to have_button('Sign in')
+  context 'when a user is not logged in' do
+    scenario 'it has a login link' do
+      visit root_path
+      expect(page).to have_button('Sign in')
+    end
   end
 
 end
